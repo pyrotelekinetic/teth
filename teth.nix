880037efa0cb0bd08ae1683ab2d676304733bbcf
Carter Ison <carter@isons.org>
2023-09-22 02:55:58 -0700

    Add lens and core-text (rope)  dependencies
    Use 'OverloadedStrings' for ropes.

--- teth.nix
+++ teth.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, brick, lib, text, vty }:
+{ mkDerivation, base, brick, core-text, lens, lib, text, vty }:
 mkDerivation {
   pname = "teth";
   version = "0.0.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base brick text vty ];
+  executableHaskellDepends = [ base brick core-text lens text vty ];
   description = "A text editor in haskell";
   license = lib.licenses.agpl3Plus;
   mainProgram = "teth";
