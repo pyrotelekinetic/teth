74c4b30bc8a2f14a9b4b06761803b1db54f9e4c4
Carter Ison <carter@isons.org>
2023-09-30 17:54:20 -0700

    Implement trivial app with Vty
    Also switch to Yi.Rope.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,22 @@
 module Main where
 
-import Core.Text.Rope
+import System.Environment (getArgs)
 import Graphics.Vty
+import Yi.Rope as R
+import Data.Text
+
+type Rope = YiString
 
 main :: IO ()
 main = do
+  (file : _) <- getArgs
+  print file
   vty <- mkVty =<< standardIOConfig
-  update vty . picForImage . string defAttr $ fromRope "Hello, Haskell!"
+  render vty file
   nextEvent vty
   shutdown vty
+
+render :: Vty -> FilePath -> IO ()
+render v p = R.readFile p >>= \case
+  Left e -> error $ unpack e
+  Right r -> update v . picForImage . text' defAttr $ toText r
--- teth.nix
+++ teth.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, core-text, lib, vty }:
+{ mkDerivation, base, lib, text, vty, yi-rope }:
 mkDerivation {
   pname = "teth";
   version = "0.0.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base core-text vty ];
+  executableHaskellDepends = [ base text vty yi-rope ];
   description = "A text editor in haskell";
   license = lib.licenses.agpl3Plus;
   mainProgram = "teth";
